4ae0f942fa3ed23567bc3d52a20a09bda4e7f7ab
Anthony Marino <ant.marino2@gmail.com>
2024-12-02 14:20:16 -0600

    Routing from Home to Meat file set up

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home'; // Ensure the path to Home.jsx is correct
+import Home from './pages/Home/HomePage'; // Ensure the path to HomePage.jsx is correct
+import Meat from './pages/Food/meat';
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Route to render the Home component when at the root URL */}
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/meat' element={<Meat />} />
+        
       </Routes>
     </Router>
   );
