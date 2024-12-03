3fbc7753ba7b4ff9470783ba9a00cb38c39fe131
jbraha12 <jbraha1@lsu.edu>
2024-12-03 08:51:50 -0600

    website/db

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$con=mysqli_connect("localhost","root","","grocerydb"); 
-if(!$con) 
-{ 
-	die("cannot connect to server"); 
-}	 
-?>
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    return(
+        <header>
+            <h1>Costgeaux Grocery Store Inventory</h1>
+            <nav>
+
+            </nav>
+        </header>
+    );
+}
+
+export default Header
