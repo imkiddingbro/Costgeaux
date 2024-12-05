e9734c3b3bd069cc23cc5650bcf6267338e67259
Anthony Marino <ant.marino2@gmail.com>
2024-12-05 11:36:00 -0600

    Fixed repeated prompt issue in script

--- backend/server.js
+++ backend/server.js
@@ -26,29 +26,55 @@ db.connect((err) => {
       "You are a manager looking to update one of your employees states because they recently moved."
     );
 
+    let question1a;
+    let question1b;
+    let question1c;
+    let question1d;
+
     // Prompt user for input
-    var question1a = prompt("Input the employees name.\n");
-    var question1b = prompt("Input the employees new city.\n");
-    var question1c = prompt("Input the employees state.\n");
-    var question1d = prompt("Input the employees position.\n");
-
-    const updateQuery =
-      "UPDATE Employee SET e_City = ?, e_State = ?, e_Position = ? WHERE e_name = ?";
-    db.query(
-      updateQuery,
-      [question1b, question1c, question1d, question1a],
-      (err, results) => {
-        if (err) {
-          console.error("Failed to update employee record:", err);
-        } else if (results.affectedRows === 0) {
-          console.log("No employee found with the given name.");
-        } else {
-          console.log("Employee record updated successfully!");
-        }
-
-        // Close the database connection
-        db.end();
+    while (!question1a && !question1b && !question1c && !question1d) {
+      question1a = prompt("Input the employees name (EX: 'Stacy'): ");
+      if (question1a) {
+        question1b = prompt("Input the employees new city (EX: 'Dallas'): ");
+      } else {
+        console.log("Invalid Input.");
       }
-    );
+      if (question1b) {
+        question1c = prompt("Input the employees state (EX: 'TX'): ");
+      } else {
+        console.log("Invalid Input.");
+      }
+      if (question1c) {
+        question1d = prompt("Input the employees position (EX: 'employee'): ");
+      } else {
+        console.log("Invalid Input.");
+      }
+      if (question1d) {
+        const updateQuery1 =
+          "UPDATE Employee SET e_City = ?, e_State = ? WHERE e_name = ? AND e_Position = ?";
+        db.query(
+          updateQuery1,
+          [question1b, question1c, question1a, question1d],
+          (err, results) => {
+            if (err) {
+              console.error("Failed to update employee record:", err);
+            } else if (results.affectedRows === 0) {
+              console.log(
+                "No employee found with the given name and position."
+              );
+            } else {
+              console.log("Employee record updated successfully!");
+            }
+
+            // Close the database connection
+            db.end();
+          }
+        );
+      } else {
+        console.log("Invalid Input");
+      }
+    }
   }
+  // Close the database connection
+  db.end();
 });
